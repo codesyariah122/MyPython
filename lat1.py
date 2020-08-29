1bf16bd50a04c3d9d2706914a40078bbeaf1fc14
codesyariah122 <pujiermanto@gmail.com>
2020-08-30 06:34:29 +0700

    🧀

---
+++ lat1.py
@@ -0,0 +1,20 @@
+# variable = "Isi variable"
+# variable2 = 20
+
+# print (variable)
+
+# print (variable2)
+
+# del(variable2)
+
+# print(variable2)
+
+nama = "Puji Ermanto"
+umur = 32
+tinggi = 170.5
+
+print(nama)
+
+namaType = type(nama)
+
+print(namaType)
---
+++ lat2.py
@@ -0,0 +1,18 @@
+import time 
+start_time = time.time()
+
+print("Hello")
+print("World")
+print("Hello World")
+
+print("Hallo Dunia")
+
+a = 10
+
+""" ada apa dengan dirimu
+yah gitu deh ini namanya komentar
+"""
+
+print(a)
+
+print(time.time()-start_time, "detik")
