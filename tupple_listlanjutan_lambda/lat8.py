1fc1cd28acf5f291ec86c27eaadef8ed37774830
codesyariah122 <pujiermanto@gmail.com>
2020-08-31 16:58:18 +0700

    🏖

---
+++ tupple_listlanjutan_lambda/lat8.py
@@ -0,0 +1,14 @@
+# simpan angka 3 dalam bentuk biner di biner_saya
+biner_saya = 0b11
+
+print(biner_saya)
+
+# simpan angka 10 dalam bentuk oktal di oktal_saya
+oktal_saya = 0x12
+
+print(oktal_saya)
+
+# simpan angka 19 dalam bentuk heksa di heksa_saya
+heksa_saya = 0x13
+
+print(heksa_saya)
---
+++ tupple_listlanjutan_lambda/lat9.py
@@ -0,0 +1,12 @@
+print('Ha'*5)
+print([1, 2] * 5)
+print(1, 2 * 5)
+
+a = range(12)
+
+print(a[::2])
+print(a[::-1])
+
+a = [6, 3, 2, 5, 4, 1]
+a.sort()
+print(a) 
