864d4357e11dcfbb8a40692f69d8a1a1e26c4c20
codesyariah122 <pujiermanto@gmail.com>
2020-08-31 17:01:30 +0700

    🍱

--- tupple_listlanjutan_lambda/lat9.py
+++ tupple_listlanjutan_lambda/lat9.py
@@ -9,4 +9,13 @@ print(a[::-1])
 
 a = [6, 3, 2, 5, 4, 1]
 a.sort()
-print(a) 
+print(a)
+
+print({'kuadrat_'+str(x): x**2 for x in range(5)})
+
+a = 5
+b = 7
+
+print('Sebelum: a = '+str(a)+', b = '+str(b))
+a, b = b, a
+print('Sesudah: a = '+str(a)+', b = '+str(b)) 
