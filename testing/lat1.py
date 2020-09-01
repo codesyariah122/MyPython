8e7594e18819f6e87c9e377cb5dfb7e1761313f7
codesyariah122 <pujiermanto@gmail.com>
2020-09-01 15:50:50 +0700

    🏇

---
+++ testing/lat1.py
@@ -0,0 +1,12 @@
+print("Masuan dua buah angka.")
+print("Dan check hubungan kedua angka tersebut")
+
+angka1 = input("Masukan angka pertama : ")
+
+angka2 = input("Masukan angka kedua : ")
+
+if angka1 == angka2:
+	print("{} sama dengan {}".format(angka1, angka2))
+
+else:
+	print("{} tidak sama dengan {}".format(angka1, angka2))
