8de705a1a9a987a0f0042b25d2c61a271a589976
codesyariah122 <pujiermanto@gmail.com>
2020-09-03 09:45:56 +0700

    🦁

---
+++ projectdjango/Env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/puji122/projectdjango/Env/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ projectdjango/mywebsite/mywebsite/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+	return render(request, 'index.html')
+
+def about(request):
+	return render(request, 'about.html')
